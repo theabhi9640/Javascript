27494751301b7cf6ca1bf038b37a6ac3881a2d79
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-10-18 23:44:42 +0530

    LC-2 Statement and Comments

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-// var digit1 = 5;
-
-/*for use variable 
-alert(digit1) ;*/
-
-/*for use string use quote 
-alert("digit1");*/
-
-/*when two variables assign 
-var digit1 = 10;
-var x = 2;
-var z = digit1 + x;
-console.log(z);*/
+var a;
+a = 5;
+// this will popup 5 this is sing line comment
 
 
-/*use let and const
-let digit1 = 10;
-const x = 2;
-
-digit1 = 20;
-// x = 5;
-console.log(x);*/
-
-// two variables can assign 
-let x = 5,
-y = "abhishek";
-
-console.log(y);
+/*this 
+is multiline 
+comment*/
+alert(a); 
