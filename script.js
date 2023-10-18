9cc327553e60c1c4868dfa11ccd1338028e85e69
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-10-18 23:42:11 +0530

    LC-3 Variables(let,const,var)

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-var a;
-a = 5;
-// this will popup 5 this is sing line comment
+// var digit1 = 5;
+
+/*for use variable 
+alert(digit1) ;*/
+
+/*for use string use quote 
+alert("digit1");*/
+
+/*when two variables assign 
+var digit1 = 10;
+var x = 2;
+var z = digit1 + x;
+console.log(z);*/
 
 
-/*this 
-is multiline 
-comment*/
-alert(a); 
+/*use let and const
+let digit1 = 10;
+const x = 2;
+
+digit1 = 20;
+// x = 5;
+console.log(x);*/
+
+// two variables can assign 
+let x = 5,
+y = "abhishek";
+
+console.log(y);
