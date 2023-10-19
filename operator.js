c71a9400a51a149f0339730c1e1072705a2a2631
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-10-19 16:13:16 +0530

    LC-4 Operators

---
+++ operator.js
@@ -0,0 +1,39 @@
+/*Assignment operator
+let x;
+x =10;
+let y = 3;
+x = y;
+console.log(x);*/
+
+
+ /*Addition operator
+let x;
+x = 10;
+let y = 3;
+let z;
+z = x + y;
+console.log(z);*/
+
+ /*modular operator for reminder 
+let x;
+x = 10;
+let y = 3;
+let z;
+z = x % 3;
+console.log(z);*/
+
+/*For power
+let x;         
+x = 5;
+let y = 3;
+let z;
+z = x**3;
+console.log(z);*/
+
+/*how to add two string 
+let x = 5; //number converted into string
+let y = "5"; //string
+
+let a = "hello";
+let b = "world";
+console.log(a+" "+b);*/
