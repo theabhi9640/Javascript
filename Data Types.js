2fb6de190170a82f8dfa7ecb9c9f62d148be1d20
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-10-19 23:46:32 +0530

    LC-5 Data Types

---
+++ Data Types.js
@@ -0,0 +1,22 @@
+/*string data type 
+let x= 'hel\'lo';
+console.log(x);
+
+
+let y = "hel\"lo";
+console.log(y);*/
+
+/*number data type 
+let x = -34.30;
+console.log(x);*/
+
+
+/* boolen data type 
+let x = 2<3;
+console.log(x);*/
+
+
+// two string with one 
+let x = "Abhishek";
+alert("hello " + x);
+ 
