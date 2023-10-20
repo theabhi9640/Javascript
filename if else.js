6b102bcddbee7aa0c3945096dd320104f81ea111
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-10-20 16:57:01 +0530

    LC-6 If Else Condition

---
+++ if else.js
@@ -0,0 +1,35 @@
+/*let age = 15;
+let hasvotercard = 'Yes';
+
+if (age>=14){
+
+    if (hasvotercard == 'Yes'){
+        alert("You can vote!");
+    }else{
+        alert("Get Your voter id card");
+    }
+
+}else{
+
+    alert('You can not vote');
+}*/
+
+/*let isLoggedin = 1; //if 0 loggedout if 1 login
+
+if(isLoggedin == 0){
+    document.write('Login');
+}else{
+    document.write('Logout');
+}*/
+
+/*Ternary Operator
+let isLoggedin = 1;
+let option = isLoggedin == 1 ? "logout" : "login";
+            // condition             true      false
+            document.write(option);*/
+
+//  Null Squezing Operator
+ let user;
+//  user = "Abhishek";
+ alert(user ?? "Guest User");
+ 
