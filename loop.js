a3bf4f5098ef00aca8965eae3c57370d5478cb20
Abhishek <144846065+theabhi9640@users.noreply.github.com>
2023-10-21 22:47:43 +0530

    LC-8 Loop

---
+++ loop.js
@@ -0,0 +1,35 @@
+/*While loop
+ let counter = 1;
+ while (counter <= 10){
+    document.write("Abhishek");
+    counter++;
+ }*/
+
+
+ /*while loop
+let counter = 1;
+let sum = 0;
+while(counter <= 1000){
+if(counter % 2 == 0){
+   sum = sum + counter;
+  }
+  counter ++;
+}
+document.write(sum);*/
+
+/*do While loop
+ let counter = 12;
+
+ do{
+    document.write("Abhishek");
+    counter++;
+ } while (counter <= 10);*/
+
+
+//  for loop
+
+ for( let counter = 1; counter <= 10; counter++) {
+    document.write("Abhishek");
+ } 
+ 
+
